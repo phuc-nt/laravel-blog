6756e002e76356724887148dee9dc323dc9d3e40
PHUC Nguyen Trong <phucnt@PHUCs-MacBook-Air.local>
2017-10-14 09:34:32 +0900

    change library from dynamic -> static

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -5,7 +5,10 @@
 <title>Phucnt @yield('title')</title> <!-- CHANGE THIS TITLE FOR EACH PAGE -->
 
 <!-- Bootstrap -->
+<!--
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+-->
+{!! Html::style('css/bootstrap.min.css') !!}
 
 @yield('stylesheets')
 
--- resources/views/partials/_javascripts.blade.php
+++ resources/views/partials/_javascripts.blade.php
@@ -1,6 +1,12 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!--
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+-->
+{!! Html::script('js/jquery.min.js') !!}
 <!-- Include all compiled plugins (below), or include individual files as needed -->
+<!--
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+-->
+{!! Html::script('js/bootstrap.min.js') !!}
 
 @yield('scripts')
